86dd4af220e102ab6317046584b6f524fae1cf58
hex4 <121138331+Hex-4@users.noreply.github.com>
2025-04-21 20:02:07 -0600

    [BROKEN] add color choosing

--- src/components/dot.svelte
+++ src/components/dot.svelte
@@ -9,27 +9,36 @@
 		console.log(popover);
 		popover.showPopover();
 	}
+
+	let color = $state()
+
+	if (dotNum < nowDots) {
+		color = "bg-text"
+	} else if (dotNum == nowDots) {
+		color = "bg-primary"
+	} else {
+		color = "bg-inactive"
+	}
+
 </script>
 
 {#snippet picker()}
-	<div popover class="bg-inactive border-text rounded-xl border-1 p-4" bind:this={popover}>
-		<ColorPicker components={ChromeVariant} sliderDirection="horizontal" isDialog={false} />
+	<div popover class="bg-bg border-text rounded-xl border-1 p-4" bind:this={popover}>
+		<div class="flex flex-row gap-1">
+			<div class="w-3 h-6 rounded-full hover:w-6 transition-all bg-red" onclick={() => { color = "bg-red"}}></div>
+			<div class="w-3 h-6 rounded-full hover:w-6 transition-all bg-yellow" onclick={() => { color = "bg-yellow"}}></div>
+			<div class="w-3 h-6 rounded-full hover:w-6 transition-all bg-green" onclick={() => { color = "bg-green"}}></div>
+			<div class="w-3 h-6 rounded-full hover:w-6 transition-all bg-blue" onclick={() => { color = "bg-blue"}}></div>
+			<div class="w-3 h-6 rounded-full hover:w-6 transition-all bg-primary" onclick={() => { color = "bg-primary"}}></div>
+			<div class="w-3 h-6 rounded-full hover:w-6 transition-all bg-inactive" onclick={() => { color = "bg-inactive"}}></div>
+		</div>
 	</div>
 {/snippet}
 
-{#if dotNum < nowDots}
-	<button onclick={openPopover} aria-label="Open Dot Dialog">
-		<div class="bg-text dot size-6 rounded-full"></div>
-	</button>
-	{@render picker()}
-{:else if dotNum == nowDots}
-	<button onclick={openPopover} aria-label="Open Dot Dialog">
-		<div class="bg-primary dot size-6 rounded-full"></div>
-	</button>
-	{@render picker()}
-{:else}
-	<button onclick={openPopover} aria-label="Open Dot Dialog">
-		<div class="bg-inactive dot size-6 rounded-full"></div>
-	</button>
-	{@render picker()}
-{/if}
+	<div id = "test">
+		<button onclick={openPopover} aria-label="Open Dot Dialog">
+			<div class={["dot size-6 rounded-full", color]}></div>
+			{@render picker()}
+		</button>
+		{@render picker()}
+	</div>
